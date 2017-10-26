0cc32b9b89f7d03305a7a9b6ce6755d546a91f64
ebraun99 <ericbraun@outlook.com>
2017-10-26 13:35:34 -0500

    update

--- src/main/java/com/astontech/nfl/comastontechnfl/configuration/SecurityConfiguration.java
+++ src/main/java/com/astontech/nfl/comastontechnfl/configuration/SecurityConfiguration.java
@@ -1,27 +1,27 @@
-package com.astontech.nfl.comastontechnfl.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter
-{
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity
-                .authorizeRequests().antMatchers("/").permitAll()
-                .and()
-                .authorizeRequests().antMatchers("/console/**").permitAll();
-
+//package com.astontech.nfl.comastontechnfl.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter
+//{
+//    @Override
+//    protected void configure(HttpSecurity httpSecurity) throws Exception{
 //        httpSecurity
-//                .formLogin().loginPage("/login").loginProcessingUrl("/login.do")
-//                .defaultSuccessUrl("/", true)
-//                .failureUrl("/login?err=1")
-//                .usernameParameter("username")
-//                .passwordParameter("password");
-
-        httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
-    }
-}
+//                .authorizeRequests().antMatchers("/").permitAll()
+//                .and()
+//                .authorizeRequests().antMatchers("/console/**").permitAll();
+//
+////        httpSecurity
+////                .formLogin().loginPage("/login").loginProcessingUrl("/login.do")
+////                .defaultSuccessUrl("/", true)
+////                .failureUrl("/login?err=1")
+////                .usernameParameter("username")
+////                .passwordParameter("password");
+//
+//        httpSecurity.csrf().disable();
+//        httpSecurity.headers().frameOptions().disable();
+//    }
+//}
---
+++ src/main/webapp/WEB-INF/home.jsp
@@ -0,0 +1,8 @@
+<%--<link rel="import" href="../templates/navbar.jsp">--%>
+<%@include file="navbar.jsp"%>
+
+
+
+<%--<footer>--%>
+    <%--<link rel="import" href="../templates/footer.html">--%>
+<%--</footer>--%>
